7d7ad0fddf42dec230a76d375904226a578cdf01
lamisreal <caothanhlam29101999@gmail.com>
2025-05-16 07:53:54 +0700

    update: sql

--- client/models/create.sql
+++ client/models/create.sql
@@ -26,7 +26,55 @@ CREATE TABLE login_sessions (
     PRIMARY KEY (id, username, device_uuid)
 );
 
+CREATE TABLE unit (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    unit_code VARCHAR(4) NOT NULL,
+    unit_name VARCHAR(30) NOT NULL,
+    create_by VARCHAR(16) NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_by VARCHAR(16),
+    update_at TIMESTAMP,
+    validflag VARCHAR(1) NOT NULL DEFAULT '1'
+); -- unit_name = "Người"
+
+CREATE TABLE captinh (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    province_code VARCHAR(4) NOT NULL,
+    province_name VARCHAR(30) NOT NULL,
+    create_by VARCHAR(16) NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_by VARCHAR(16),
+    update_at TIMESTAMP,
+    validflag VARCHAR(1) NOT NULL DEFAULT '1'
+);
+
+CREATE TABLE dansotrungbinh (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    province_code VARCHAR(4) NOT NULL,
+    unit_code VARCHAR(4) NOT NULL DEFAULT "HUMA",
+    info_type VARCHAR(4) DEFAULT "DSTB", 
+    create_by VARCHAR(16) NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_by VARCHAR(16),
+    update_at TIMESTAMP,
+    validflag VARCHAR(1) NOT NULL DEFAULT '1'
+);
+
+CREATE TABLE thongtinsolieu (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    province_code VARCHAR(4) NOT NULL,
+    info_type VARCHAR(4) NOT NULL,
+    info_year INT,
+    info_quantity DECIMAL(16),
+    create_by VARCHAR(16) NOT NULL,
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    update_by VARCHAR(16),
+    update_at TIMESTAMP,
+    validflag VARCHAR(1) NOT NULL DEFAULT '1'
+);
+
 ALTER TABLE information CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE captinh CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 --start
 -- truy cập: https://www.phpmyadmin.co/db_sql.php?db=sql12778065
@@ -38,7 +86,12 @@ delete from `login_sessions`
 INSERT INTO superadmin (username, email, role, password) VALUES
 ('superadmin1', '23410165@ms.uit.edu.com', 'superadmin', 'e10adc3949ba59abbe56e057f20f883e'),
 ('superadmin2', '23410154@ms.uit.edu.com', 'superadmin', 'e10adc3949ba59abbe56e057f20f883e');
+('admin1', 'admin1@ms.uit.edu.com', 'admin', 'e10adc3949ba59abbe56e057f20f883e');
 
 INSERT INTO information (username, hoten, gioitinh, ngsinh) VALUES
 ('superadmin1', 'Cao Thanh Lâm', 'M', '1991-10-01 00:00:00'),
 ('superadmin2', 'Cao Trung Hiếu', 'M', '1995-05-20 00:00:00');
+('admin1', 'admin1', 'M', '1997-12-20 00:00:00');
+
+INSERT INTO unit (unit_code, unit_name, create_by) VALUES 
+( 'HUMA', 'Người', 'admin1');
